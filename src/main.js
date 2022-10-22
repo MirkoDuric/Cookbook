7d5b304e8d810763ff74b0cc1023c943aeeddcef
Trygve Horn <trygvehorn@gmail.com>
2022-10-22 13:33:28 +0200

    first part of resize code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
- 
+// add onclick="cardReposition () to card div
+function cardReposition () {
+    console.log("card click trigger working");
+    document.getElementById("blogCard1").style.gridRowStart = "1";
+    document.getElementById("blogCard1").style.gridColumnStart = "1"; 
+    document.getElementById("blogCard1").style.gridRowEnd = "-1";
+    document.getElementById("blogCard1").style.gridColumnEnd = "-1";
+}
