cf72f854839fe6c97f7b57a8c0c44956e5c5b33d
Trygve Horn <trygvehorn@gmail.com>
2022-10-22 17:35:34 +0200

    jsAdjustment

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ function cardReposition () {
     console.log("card click trigger working");
     document.getElementById("blogCard1").style.gridRowStart = "1";
     document.getElementById("blogCard1").style.gridColumnStart = "1"; 
-    document.getElementById("blogCard1").style.gridRowEnd = "-1";
-    document.getElementById("blogCard1").style.gridColumnEnd = "-1";
+    document.getElementById("card").style.height = "700px";
+    document.getElementById("card").style.width = "100%";
 }
 
 function killCard () {
