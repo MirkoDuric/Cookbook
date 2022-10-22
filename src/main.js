651db05f36d9522bb0ba24609569f02166077455
Trygve Horn <trygvehorn@gmail.com>
2022-10-22 17:51:01 +0200

    glitchyDemo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // add onclick="cardReposition () to card button
 function cardReposition () {
     console.log("card click trigger working");
-    document.getElementById("blogCard1").style.gridRowStart = "1";
-    document.getElementById("blogCard1").style.gridColumnStart = "1"; 
+    // document.getElementById("card").style.gridRowStart = "1";
+    // document.getElementById("card").style.gridColumnStart = "1"; 
     document.getElementById("card").style.height = "700px";
     document.getElementById("card").style.width = "100%";
 }
