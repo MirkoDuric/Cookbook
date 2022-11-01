477f367d27fbbc0b306592cccbb2ecaebc4bc44d
MirkoDuric <mirkodjuricczv@gmail.com>
2022-11-01 14:12:52 +0100

    Resize cards function finished. Repositioning left out on purpose

--- src/main.js
+++ src/main.js
@@ -1,67 +1,61 @@
-// identify buttonID, establish cardID, inject css
+// // identify buttonID, establish cardID, inject css
 // let cardOrder = -1;
 // const onClick = (event) => {
-    // establish Card ID
-    // if (event.target.nodeName === 'BUTTON') {
-        // console.log(event.target.id);
-        // const cardID = "card" + event.target.id;
-        // console.log(cardID);
-        // reposition Card
-        // console.log(cardOrder);
-        // document.getElementById(cardID).style.order = cardOrder;
-        // cardOrder--
-        // console.log(cardOrder);
-        // resize Card
-        // document.getElementById(cardID).style.height = "700px";
-        // document.getElementById(cardID).style.width = "100%";
-            // reformat card
-                // document.getElementById(cardID).style.display = "flex"
-                // document.getElementById(cardID).style.alignItems = "flex-start"
-            // create method and ingredients sections
-                // var tag = document.createElement("div");
-                // var textIngredients = ("Ingredients");
-                // tag.appendChild(textIngredients);
-                // var element = document.getElementById(cardID);
-                // element.appendChild(tag);
-  //   }
-  // }
-  // window.addEventListener('click', onClick);
+//     // establish Card ID
+//     if (event.target.nodeName === 'BUTTON') {
+//         console.log(event.target.id);
+//         const cardID = "card" + event.target.id;
+//         console.log(cardID);
+//         // reposition Card
+//         console.log(cardOrder);
+//         document.getElementById(cardID).style.order = cardOrder;
+//         cardOrder--
+//         console.log(cardOrder);
+//         // resize Card
+//         document.getElementById(cardID).style.height = "700px";
+//         document.getElementById(cardID).style.width = "100%";
+//             // reformat card
+//                 document.getElementById(cardID).style.display = "flex"
+//                 document.getElementById(cardID).style.alignItems = "flex-start"
+//             // create method and ingredients sections
+//                 var tag = document.createElement("div");
+//                 var textIngredients = ("Ingredients");
+//                 tag.appendChild(textIngredients);
+//                 var element = document.getElementById(cardID);
+//                 element.appendChild(tag);
+//     }
+//   }
+//   window.addEventListener('click', onClick);
 
+// RESIZING FUNCTION
 const cards = document.querySelectorAll(".resizeCard");
-let cardWidth = cards.offsetWidth;
-let cardHeight = cards.offsetHeight;
 const recipeButtons =  document.querySelectorAll(".recipeButton");
 
 function resizingCard(e){
-  if(!cardWidth >= "70%"){
-    console.log('if condition ok')
-    for(let i=0; i<cards.length; i++){
-      e.target.parentNode.parentNode.style.width = "100%";
-      e.target.parentNode.parentNode.style.height = "700px";
-    }
+  const classesForBigCards = 'card text-center col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 resizeCard expanded';
+  const classesForSmalCards = 'card text-center col-10 col-sm-10 col-md-5 col-lg-3 col-xl-3';
+  const cardParentDiv = e.target.parentNode.parentNode;
+  let cardHeight =  cardParentDiv.offsetHeight;
+  console.log(cardHeight);
+  console.log(cardParentDiv.offsetWidth);
+  if(!cardParentDiv.classList.contains('expanded')){
+      console.log('if condition ok')
+      cardParentDiv.style.height = "700px";
+      cardParentDiv.className = classesForBigCards;
   }else {
     console.log('else condition ok')
-    for(let i=0; i<cards.length; i++){
-      e.target.parentNode.parentNode.style.width = "42%";
-    }
+      cardParentDiv.className = classesForSmalCards;
+      cardParentDiv.style.height = "448px";
   }
 };
+
 for(let i = 0; i < recipeButtons.length; i++){
   recipeButtons[i].addEventListener('click', function(e){
     console.log('Event Ok')
     resizingCard(e);
   })
 }
-
-
-
-
-  
-// function killCard () {
-//     console.log("killcard triggered");
-//     var elem = document.getElementById("killme");
-//     elem.remove();
-// }
+// ALERT MESSAGE
 const buttons = document.querySelectorAll(".shareButton");
 function alertMessage(){
   alert('Share our recipe!')
@@ -72,3 +66,8 @@ function alertMessage(){
        alertMessage();
      });
  }
+ // function killCard () {
+//     console.log("killcard triggered");
+//     var elem = document.getElementById("killme");
+//     elem.remove();
+// }
