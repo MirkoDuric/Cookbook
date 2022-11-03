cd24c5cb873231e5a548fb47fc79d0e19f152a32
Trygve Horn <trygvehorn@gmail.com>
2022-11-03 15:32:04 +0100

    save

--- src/main.js
+++ src/main.js
@@ -7,36 +7,28 @@ const onClick = (event) => {
         // reposition Card and decrement cardOrder
         document.getElementById(cardID).style.order = cardOrder;
         cardOrder--;
-
-
-
-                                // move card into grid
-                                //document.getElementById(cardID).insertAdjacentElement
-
-
-
         // resize Card
         document.getElementById(cardID).style.height = "700px";
         document.getElementById(cardID).style.width = "100%";
 
         
                                 //add new grid item
-                                const flexIngredientsDiv = document.createElement("div");
-                                flexIngredientsDiv.id = "flexIngredientsDiv";
-                                document.body.appendChild(flexIngredientsDiv);
-                                document.getElementById(cardID).appendChild(flexIngredientsDiv);
+                                // const flexIngredientsDiv = document.createElement("div");
+                                // flexIngredientsDiv.id = "flexIngredientsDiv";
+                                // document.body.appendChild(flexIngredientsDiv);
+                                // document.getElementById(cardID).appendChild(flexIngredientsDiv);
 
-                                const flexIngredients = document.createElement("flexIngredients");
-                                flexIngredients.innerText = "Button";
-                                flexIngredients.id = "flexIngredients-1";
-                                flexIngredientsDiv.appendChild(flexIngredients);
+                                // const flexIngredients = document.createElement("flexIngredients");
+                                // flexIngredients.innerText = "Button";
+                                // flexIngredients.id = "flexIngredients-1";
+                                // flexIngredientsDiv.appendChild(flexIngredients);
 
-        // reformat card
-        document.getElementById(cardID).parentElement.style.display = "grid";
-        document.getElementById(cardID).style.gridTemplateColumns = "auto 1fr";
-        document.getElementById(cardID).style.gridTemplateRows = "1fr 1fr";
-        document.getElementById(cardID).style.gridColumnStart = "1";
-        document.getElementById(cardID).style.gridRowStart = "1";
+                                // reformat card
+                                // document.getElementById(cardID).parentElement.style.display = "grid";
+                                // document.getElementById(cardID).style.gridTemplateColumns = "auto 1fr";
+                                // document.getElementById(cardID).style.gridTemplateRows = "1fr 1fr";
+                                // document.getElementById(cardID).style.gridColumnStart = "1";
+                                // document.getElementById(cardID).style.gridRowStart = "1";
 
     }
   }
