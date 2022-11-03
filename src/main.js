26fe5c32175b3658068a49251508a9b7d41ebe2f
Trygve Horn <trygvehorn@gmail.com>
2022-11-03 21:07:17 +0100

    card moves into grid

--- src/main.js
+++ src/main.js
@@ -5,20 +5,34 @@ const onClick = (event) => {
         // establish Card ID
         const cardID = "card" + event.target.id;
         // reposition Card and decrement cardOrder
-        document.getElementById(cardID).style.order = cardOrder;
-        cardOrder--;
+        // document.getElementById(cardID).style.order = cardOrder;
+        // cardOrder--;
         // resize Card
-        document.getElementById(cardID).style.height = "700px";
-        document.getElementById(cardID).style.width = "100%";
+        //document.getElementById(cardID).style.height = "700px";
+        //document.getElementById(cardID).style.width = "100%";
 
         //Mirko, this is the 1st version of the element creation.
-        let div = document.createElement('div');
-        div.id = 'tempGrid';
-        div.innerHTML = '<p>CreateElement example</p>';
-        document.body.appendChild(div);
-        document.getElementById(cardID).appendChild(div);
+                    // let div = document.createElement('div');
+                    // div.id = 'tempGrid';
+                    // div.innerHTML = '<p>CreateElement example </p>';
+                    // document.body.appendChild(div);
+                    // document.getElementById(cardID).parentElement.appendChild(div);
+        // shifting to another approach...
+
+        document.getElementById('gridContainer').style.gridTemplateColumns = "222px 1fr";
+        document.getElementById('gridContainer').style.gridTemplateRows = "450px 333px";
+        const containerGrid = document.getElementById('gridContainer');
+        const card = document.getElementById(cardID);
+        containerGrid.appendChild(card); // move complete
+        document.getElementById(cardID).style.width = "100%";
+        //document.getElementById(cardID).style.gridColumnStart = "1";
+
 
         
+
+
+
+
                                 //add new grid item
                                 // const flexIngredientsDiv = document.createElement("div");
                                 // flexIngredientsDiv.id = "flexIngredientsDiv";
